893b3412388df37daa4c31e4e883835caa8ee054
Yerong <yerong.li@outlook.com>
2023-05-13 22:28:35 -0500

    print the keys of the graph set dictionary

--- Graph_Toolformer_Package/load.py
+++ Graph_Toolformer_Package/load.py
@@ -5,4 +5,4 @@ f.close()
 print(dataset.keys())
 # print(dataset['data_profile'])
 # {'name': 'gpr', 'graph_number': 37, 'is_directed': False, 'is_weighted': False}
-print(dataset['graph_set'])
+print(dataset['graph_set'].keys())
