c41ffaac3f14362dbaddc9e0f270d95dd1d06e42
Yerong <yerong.li@outlook.com>
2023-05-13 22:23:48 -0500

    Print graph set

--- Graph_Toolformer_Package/load.py
+++ Graph_Toolformer_Package/load.py
@@ -3,4 +3,6 @@ f = open('./graph_datasets/gpr', 'rb')
 dataset = pickle.load(f)
 f.close()
 print(dataset.keys())
-print(dataset['data_profile'])
+# print(dataset['data_profile'])
+# {'name': 'gpr', 'graph_number': 37, 'is_directed': False, 'is_weighted': False}
+print(dataset['graph_set'])
