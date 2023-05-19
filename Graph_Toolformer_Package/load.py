7dab99217bb91d14f22e1ac7307e3de9270b7df1
Yerong <yerong.li@outlook.com>
2023-05-19 01:28:21 -0500

    Check freebase

--- Graph_Toolformer_Package/load.py
+++ Graph_Toolformer_Package/load.py
@@ -1,5 +1,5 @@
 import pickle
-f = open('./graph_datasets/gpr', 'rb')
+# f = open('./graph_datasets/gpr', 'rb')
 dataset = pickle.load(f)
 f.close()
 # print(dataset.keys())
@@ -10,3 +10,10 @@ f.close()
 
 # print(dataset['graph_set'].keys())
 # dict_keys(['dodecahedral_graph', 'desargues_graph', 'icosahedral_graph', 'binomial_tree', 'path_graph', 'house_x_graph', 'complete_graph', 'truncated_tetrahedron_graph', 'heawood_graph', 'bull_graph', 'sedgewick_maze_graph', 'turan_graph', 'hoffman_singleton_graph', 'full_rary_tree', 'circular_ladder_graph', 'star_graph', 'chvatal_graph', 'krackhardt_kite_graph', 'cycle_graph', 'ladder_graph', 'moebius_kantor_graph', 'diamond_graph', 'wheel_graph', 'tetrahedral_graph', 'house_graph', 'truncated_cube_graph', 'balanced_tree', 'tutte_graph', 'frucht_graph', 'lollipop_graph', 'cubical_graph', 'barbell_graph', 'petersen_graph', 'dorogovtsev_goltsev_mendes_graph', 'pappus_graph', 'circulant_graph', 'octahedral_graph'])
+
+
+
+f = open('./graph_datasets/freebase', 'rb')
+dataset = pickle.load(f)
+f.close()
+# print(dataset.keys())
