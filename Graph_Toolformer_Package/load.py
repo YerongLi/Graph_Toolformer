59d5a80e846b3b06f471a2a418fa1772335e3cc9
Yerong <yerong.li@outlook.com>
2023-05-19 02:18:18 -0500

    How he can make sure that he can load something correct from the freebase

--- Graph_Toolformer_Package/load.py
+++ Graph_Toolformer_Package/load.py
@@ -13,10 +13,12 @@ import pickle
 
 
 
-f = open('./graph_datasets/freebase', 'rb')
-dataset = pickle.load(f)
-f.close()
-print(dataset.keys())
+# f = open('./graph_datasets/freebase', 'rb')
+# dataset = pickle.load(f)
+# f.close()
+# print(dataset.keys())
 # dict_keys(['data_profile', 'nodes', 'links'])
-print(dataset['links'])
+# print(dataset['links'])
+# How he can make sure that he can prune anything
+# ('/m/0dgpwnk', '/m/0h7x'): {'label': ['/film/film/release_date_s./film/film_regional_release_date/film_release_region']}, ('/m/0f42nz', '/m/03f02ct'): {'label': ['/film/film/starring./film/performance/actor']}, ('/m/047myg9', '/m/01tz6vs'): {'label': ['/film/film/subjects']}, ('/m/02t4yc', '/m/019v9k'): {'label': ['/education/educational_institution/students_graduates./education/education/degree']}, ('/m/0168nq', '/m/08mbj5d'): {'label': ['/common/topic/webpage./common/webpage/category']}, ('/m/020h2v', '/m/016tt2'): {'label': ['/award/award_nominee/award_nominations./award/award_nomination/award_nominee']}, ('/m/04gnbv1', '/m/0gvstc3'): {'label': ['/award/award_winner/awards_won./award/award_honor/ceremony']}, ('/m/082gq', '/m/026qnh6'): {'label': ['/film/film_genre/films_in_this_genre']}, ('/m/05k17c', '/m/02dqdp'): {'label': ['/organization/role/leaders./organization/leadership/organization']}, ('/m/04cj79', '/m/0fvf9q'): {'label': ['/film/film/executive_produced_by']}, ('/m/09tqxt', '/m/0gtsxr4'): {'label': ['/award/award_category/nominees./award/award_nomination/nominated_for']}, ('/m/025sc50', '/m/0277c3'): {'label': ['/music/genre/artists']}, ('/m/01pjr7', '/m/02hrh1q'): {'label': ['/people/person/profession']}, ('/m/0bw20', '/m/09vw2b7'): {'label': ['/film/film/other_crew./film/film_crew_gig/film_crew_role']}, ('/m/01j4ls', '/m/08mbj5d'): {'label': ['/common/topic/webpage./common/webpage/category']}, ('/m/0cmdwwg', '/m/06t2t'): {'label': ['/film/film/release_date_s./film/film_regional_release_date/film_release_region']}, ('/m/0gs6vr', '/m/0gj96ln'): {'label': ['/film/actor/film./film/performance/film']}, ('/m/02lp0w', '/m/015882'): {'label': ['/award/award_category/nominees./award/award_nomination/award_nominee']}, ('/m/034x61', '/m/029sk'): {'label': ['/medicine/notable_person_with_medical_condition/condition']}}
 
