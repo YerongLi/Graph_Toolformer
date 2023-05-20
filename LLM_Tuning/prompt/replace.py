4139abe6208852f628c7a9340819f008ea3f45ad
Yerong <yerong.li@outlook.com>
2023-05-20 16:50:34 -0500

    Create a new dataset

---
+++ LLM_Tuning/prompt/replace.py
@@ -0,0 +1,21 @@
+def process_prompt_file(filename="knowledge_graphs/freebase/prompt_train", output_filename="knowledge_graphs/conceptnet/prompt_train"):
+    # Read input from file
+    with open(filename, "r") as file:
+        input_text = file.read()
+
+    # Perform string replacements
+    output_text = input_text.replace("According to the Freebase knowledge graph,", "According to the Concept knowledge graph,")
+    output_text = output_text.replace('GL("Freebase")', 'GL("Concept")')
+
+    # Create the output filename
+
+    # Write the output to a new file
+    with open(output_filename, "w") as file:
+        file.write(output_text)
+
+    # Return the output filename
+    return output_filename
+
+# Example usage
+output_filename = process_prompt_file()
+print(f"Output saved to: {output_filename}")
