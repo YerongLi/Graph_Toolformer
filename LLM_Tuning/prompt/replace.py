9bbd980b760feef08a9565592516bdbb161c60c4
Yerong <yerong.li@outlook.com>
2023-05-20 17:06:32 -0500

    Replace the description and the API call at the same time

--- LLM_Tuning/prompt/replace.py
+++ LLM_Tuning/prompt/replace.py
@@ -6,7 +6,7 @@ def process_prompt_file(filename="knowledge_graphs/freebase/prompts_train", outp
     # Perform string replacements
     output_text = input_text.replace("According to the Freebase knowledge graph,", "According to the Concept knowledge graph,")
     output_text = output_text.replace('GL("Freebase")', 'GL("Concept")')
-    output_text = input_text.replace("[", "<yerongAPI>")
+    output_text = output_text.replace("[", "<yerongAPI>")
     output_text = output_text.replace("]", "</yerongAPI>")
 
     # Create the output filename
