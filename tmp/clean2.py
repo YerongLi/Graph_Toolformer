a1a04a92f035670cd361cf43c3e44d545b2022e3
Yerong <yerong.li@outlook.com>
2023-06-06 21:14:34 -0500

    Remove the colon

--- tmp/clean2.py
+++ tmp/clean2.py
@@ -11,10 +11,10 @@ conversation = re.sub(r'\[(Host|Musk)\]', '', conversation)
 conversation = conversation.strip()
 
 # Replace names with tags
-conversation = re.sub(r'Peter Campbell \(PC\):', '[Host]:', conversation)
-conversation = re.sub(r'Elon Musk \(EM\):', '[Musk]:', conversation)
-conversation = re.sub(r'PC:', '[Host]:', conversation)
-conversation = re.sub(r'EM:', 'Musk:', conversation)
+conversation = re.sub(r'Peter Campbell \(PC\):', '[Host]', conversation)
+conversation = re.sub(r'Elon Musk \(EM\):', '[Musk]', conversation)
+conversation = re.sub(r'PC:', '[Host]', conversation)
+conversation = re.sub(r'EM:', '[Musk]', conversation)
 
 
 # Remove extra spaces and newlines
