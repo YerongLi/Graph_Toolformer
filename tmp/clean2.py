16eb2d5814c924859c2b9fae35f6bf88efce0b26
Yerong <yerong.li@outlook.com>
2023-06-06 21:16:58 -0500

    clean 2 : save to a file

--- tmp/clean2.py
+++ tmp/clean2.py
@@ -16,10 +16,12 @@ conversation = re.sub(r'Elon Musk \(EM\):', '[Musk]', conversation)
 conversation = re.sub(r'PC:', '[Host]', conversation)
 conversation = re.sub(r'EM:', '[Musk]', conversation)
 
-
 # Remove extra spaces and newlines
 conversation = re.sub(r'\n+', '\n', conversation)
 conversation = re.sub(r' +', ' ', conversation)
 
-# Print the cleaned up conversation
-print(conversation)
+# Save the cleaned up conversation to a file
+with open("02_clean.txt", "w") as file:
+    file.write(conversation)
+
+print("Conversation cleaned up and saved to 02_clean.txt.")
