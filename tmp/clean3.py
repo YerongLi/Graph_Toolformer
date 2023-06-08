abac95232ae98a29ff0cb5c0644382140d8bc263
Yerong <yerong.li@outlook.com>
2023-06-07 21:38:14 -0500

    Update the clean3.py

--- tmp/clean3.py
+++ tmp/clean3.py
@@ -11,41 +11,32 @@ speaker_pattern = r"^(.*?)(?: \(\d{2}:\d{2}:\d{2}\))?:"
 # Clean and format the lines
 formatted_lines = []
 current_speaker = None
-current_utterance = ""
 for line in content:
     # Remove leading/trailing whitespaces
     line = line.strip()
-    
+
     # Skip empty lines
     if not line:
         continue
-    
+
     # Match the timestamp
     timestamp_match = re.search(timestamp_pattern, line)
     if timestamp_match:
         timestamp = timestamp_match.group(1)
         line = line.replace(timestamp_match.group(0), "")
-        
+
         # Match the speaker
         speaker_match = re.search(speaker_pattern, line)
         if speaker_match:
             speaker = speaker_match.group(1)
             if speaker == "Elon Musk":
-                speaker = "Musk"
+                speaker = "[Musk]"
             else:
                 speaker = "[User]"
-                
-            # Start a new utterance if the speaker changes
-            if current_speaker and current_speaker != speaker:
-                formatted_lines.append(current_utterance.strip())
-                current_utterance = ""
-            
-            current_speaker = speaker
-            current_utterance += f"{current_speaker} ({timestamp}): {line}\n"
+
+            formatted_lines.append(f"{speaker} {line} [{speaker} ({timestamp})]")
         else:
-            current_utterance += line
-    
-    formatted_lines.append(current_utterance.strip())
+            formatted_lines.append(line)
 
 # Save the cleaned content to a new file
 with open("clean_03.txt", "w") as file:
