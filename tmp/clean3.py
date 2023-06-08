79a8cd22049058fd97d893ddf7d1f9ff2b97f98b
Yerong <yerong.li@outlook.com>
2023-06-07 22:08:34 -0500

    Update

--- tmp/clean3.py
+++ tmp/clean3.py
@@ -38,6 +38,7 @@ for i, line in enumerate(content):
         current_line += " " + line
     else:
         current_line += " " + line
+    if i > 6: break
     print(current_line)
 if current_line:
     formatted_lines.append(current_line.strip())
