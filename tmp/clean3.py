e5d9f5f4d48af01590f1e5d01a293c0bc974253f
Yerong <yerong.li@outlook.com>
2023-06-07 21:44:13 -0500

    Remove the Speaker pattern

--- tmp/clean3.py
+++ tmp/clean3.py
@@ -4,14 +4,11 @@ import re
 with open("03.txt", "r") as file:
     content = file.readlines()
 
-# Regular expression patterns for matching timestamps and speakers
+# Regular expression pattern for matching timestamps
 timestamp_pattern = r"\((\d{2}:\d{2}:\d{2})\):"
-speaker_pattern = r"^(.*?)(?: \(\d{2}:\d{2}:\d{2}\))?:"
 
 # Clean and format the lines
 formatted_lines = []
-current_speaker = None
-current_utterance = ""
 for line in content:
     # Remove leading/trailing whitespaces
     line = line.strip()
@@ -26,26 +23,17 @@ for line in content:
         timestamp = timestamp_match.group(1)
         line = line.replace(timestamp_match.group(0), "")
 
-        # Match the speaker
-        speaker_match = re.search(speaker_pattern, line)
-        if speaker_match:
-            speaker = speaker_match.group(1)
-            if speaker == "Elon Musk":
-                speaker = "[Musk]"
-            else:
-                speaker = "[User]"
-
-            # Start a new utterance if the speaker changes
-            if current_speaker and current_speaker != speaker:
-                formatted_lines.append(current_utterance.strip())
-                current_utterance = ""
-
-            current_speaker = speaker
-            current_utterance += f"{current_speaker} {line}\n"
+        # Extract the speaker from the line
+        if "Elon Musk" in line:
+            speaker = "[Musk]"
         else:
-            current_utterance += line
+            speaker = "[User]"
 
-formatted_lines.append(current_utterance.strip())
+        formatted_line = f"{speaker} {line} ({timestamp})"
+        formatted_lines.append(formatted_line)
+    else:
+        # Append the line to the previous speaker's utterance
+        formatted_lines[-1] += f" {line}"
 
 # Save the cleaned content to a new file
 with open("clean_03.txt", "w") as file:
