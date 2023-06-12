58d92f500588c9f6ec25d8329df9b1d47a94af00
Yerong <yerong.li@outlook.com>
2023-06-12 00:14:20 -0500

    Update

--- tmp/extract.py
+++ tmp/extract.py
@@ -1,8 +1,4 @@
 import cv2
-import pytesseract
-
-# Set the path to the Tesseract executable
-# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
 # Set the path to the video file
 video_path = 'discord_conversations.mp4'
@@ -10,21 +6,8 @@ video_path = 'discord_conversations.mp4'
 # Open the video file
 video = cv2.VideoCapture(video_path)
 
-# Define the text extraction function
-def extract_text_from_frame(frame):
-    # Convert the frame to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Perform text extraction using OCR
-    text = pytesseract.image_to_string(gray)
-
-    # Remove leading/trailing whitespaces and newlines
-    text = text.strip()
-
-    return text
-
-# Initialize variables to store unique extracted text
-unique_text = set()
+# Initialize a counter to keep track of the frame number
+frame_count = 0
 
 # Read frames from the video
 while True:
@@ -35,16 +18,13 @@ while True:
     if not ret:
         break
 
-    # Extract text from the frame
-    text = extract_text_from_frame(frame)
+    # Perform image processing or analysis on the frame here
+    # For example, you can save the frame as an image file
+    frame_filename = f'frame_{frame_count}.png'
+    cv2.imwrite(frame_filename, frame)
 
-    # Add the extracted text to the set of unique text
-    unique_text.add(text)
-
-# Print the unique extracted text
-for text in unique_text:
-    print(text)
+    # Increment the frame counter
+    frame_count += 1
 
 # Release the video file
 video.release()
-
