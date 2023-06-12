a2c5b234c4dfa110543c1a90db5fa5a5f81ef17f
Yerong <yerong.li@outlook.com>
2023-06-12 00:16:02 -0500

    Show progress

--- tmp/extract.py
+++ tmp/extract.py
@@ -1,13 +1,27 @@
 import cv2
+import numpy as np
+import os
+from tqdm import tqdm
 
 # Set the path to the video file
 video_path = 'discord_conversations.mp4'
+output_folder = 'frames'
+
+# Create the output folder if it doesn't exist
+os.makedirs(output_folder, exist_ok=True)
 
 # Open the video file
 video = cv2.VideoCapture(video_path)
 
-# Initialize a counter to keep track of the frame number
+# Initialize variables
+prev_frame = None
 frame_count = 0
+
+# Get total number of frames in the video
+total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+# Create tqdm progress bar
+progress_bar = tqdm(total=total_frames, unit='frames', desc='Processing Frames')
 
 # Read frames from the video
 while True:
@@ -18,13 +32,37 @@ while True:
     if not ret:
         break
 
-    # Perform image processing or analysis on the frame here
-    # For example, you can save the frame as an image file
-    frame_filename = f'frame_{frame_count}.png'
-    cv2.imwrite(frame_filename, frame)
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Increment the frame counter
-    frame_count += 1
+    # Apply thresholding to the grayscale frame
+    _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+
+    # Dilate the thresholded frame to enhance the differences
+    dilated = cv2.dilate(thresh, None, iterations=2)
+
+    # Check if there is a previous frame
+    if prev_frame is not None:
+        # Compute the absolute difference between the current and previous frames
+        diff = cv2.absdiff(dilated, prev_frame)
+
+        # Count the number of non-zero pixels in the difference image
+        diff_count = np.count_nonzero(diff)
+
+        # Save the frame only if it has significant differences from the previous frame
+        if diff_count > 1000:
+            frame_filename = os.path.join(output_folder, f'frame_{frame_count}.png')
+            cv2.imwrite(frame_filename, frame)
+            frame_count += 1
+
+    # Update the previous frame with the current frame
+    prev_frame = dilated
+
+    # Update progress bar
+    progress_bar.update(1)
 
 # Release the video file
 video.release()
+
+# Close the progress bar
+progress_bar.close()
