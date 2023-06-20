35a7a1284e25c91c9979549fe08c57d2a25ffda9
Yerong <yerong.li@outlook.com>
2023-06-20 00:31:16 -0500

    Update chatglm

---
+++ tmp/chatglm_convert.py
@@ -0,0 +1,26 @@
+import json
+
+# Open the input file
+with open("clean_01.txt", "r") as file:
+    content = file.readlines()
+
+# Extract the conversation between the user and Elon Musk
+conversation = []
+for line in content:
+    if "[User]" in line:
+        conversation.append({"role": "Human", "utterance": line.replace("[User]", "").strip()})
+    elif "[Musk]" in line:
+        conversation.append({"role": "Assistant", "utterance": line.replace("[Musk]", "").strip()})
+
+# Prepare the data in JSON format
+data = []
+for i in range(len(conversation) - 1):
+    if conversation[i]["role"] == "Human":
+        data.append({
+            "instruction": f"{conversation[i]['role']}:{conversation[i]['utterance']}",
+            "output": f"{conversation[i+1]['role']}:{conversation[i+1]['utterance']}"
+        })
+
+# Save the data to a JSON file
+with open("elon_musk.json", "w") as json_file:
+    json.dump(data, json_file, indent=4)
