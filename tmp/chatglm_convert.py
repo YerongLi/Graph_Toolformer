4e57617a84c75495f93fa2ccd4d370e558a8d878
Yerong <yerong.li@outlook.com>
2023-06-20 19:34:31 -0500

    Update the final_data

--- tmp/chatglm_convert.py
+++ tmp/chatglm_convert.py
@@ -1,31 +1,43 @@
 import json
+import os
 
-# Open the input file
-with open("clean_01.txt", "r") as file:
-    content = file.readlines()
+def extract_conversation(filename):
+    # Open the input file
+    with open(filename, "r") as file:
+        content = file.readlines()
 
-# Extract the conversation between the user and Elon Musk
-conversation = []
-for line in content:
-    if "[User]" in line:
-        conversation.append({"role": "Human", "utterance": line.replace("[User]", "").strip()})
-    elif "[Musk]" in line:
-        conversation.append({"role": "Assistant", "utterance": line.replace("[Musk]", "").strip()})
+    # Extract the conversation between the user and Elon Musk
+    conversation = []
+    for line in content:
+        if "[User]" in line:
+            conversation.append({"role": "Human", "utterance": line.replace("[User]", "").strip()})
+        elif "[Musk]" in line:
+            conversation.append({"role": "Assistant", "utterance": line.replace("[Musk]", "").strip()})
 
-# Prepare the data in JSON format
-data = []
-for i in range(len(conversation) - 1):
-    history_start_idx = max(0, i - 10)
-    history = [conv["utterance"] for conv in conversation[history_start_idx:i]]
-    instruction = conversation[i]["utterance"]
-    if conversation[i + 1]["role"] == "Human": continue
-    output = conversation[i + 1]["utterance"]
-    data.append({
-        "instruction": instruction,
-        "output": output,
-        "history": history
-    })
+    # Prepare the data in JSON format
+    data = []
+    for i in range(len(conversation) - 1):
+        history_start_idx = max(0, i - 10)
+        history = [conv["utterance"] for conv in conversation[history_start_idx:i]]
+        instruction = conversation[i]["utterance"]
+        if conversation[i + 1]["role"] == "Human": continue
+        output = conversation[i + 1]["utterance"]
+        data.append({
+            "instruction": instruction,
+            "output": output,
+            "history": history
+        })
+final_data = []
 
-# Save the data to a JSON file
-with open("elon_musk.json", "w") as json_file:
-    json.dump(data, json_file, indent=4)
+# Loop through all files in the current directory that start with "clean_" and end with ".txt"
+for filename in os.listdir():
+    if filename.startswith("clean_") and filename.endswith(".txt"):
+        # Open the file and extract the conversation data
+        with open(filename, "r") as f:
+            conversation_data = extract_conversation(f.read())
+        # Append the conversation data to the final data list
+        final_data.append(conversation_data)
+
+# Dump the final data list as a JSON file
+with open("final_data.json", "w") as f:
+    json.dump(final_data, f)
