a2567389b3e015c4d97c47b9df53ea5bee734f2e
Yerong <yerong.li@outlook.com>
2023-06-20 21:39:55 -0500

    Update the convert

--- tmp/chatglm_convert.py
+++ tmp/chatglm_convert.py
@@ -21,7 +21,7 @@ def extract_conversation(filename):
         "output": [],
         "history": []
     }
-    for i in range(len(conversation) - 1):
+    for i in range(10):
         history_start_idx = max(0, i - 10)
         history = [conv["utterance"] for conv in conversation[history_start_idx:i]]
         instruction = conversation[i]["utterance"]
