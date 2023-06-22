a1a59d8452846bb61964e2a1a416756e31dac7d7
Yerong <yerong.li@outlook.com>
2023-06-21 22:53:41 -0500

    Update check.py

--- tmp/check.py
+++ tmp/check.py
@@ -1,12 +1,23 @@
-import re
-import sys
+import glob
 
-# define the regular expression pattern to match allowed characters
-pattern = r'^[a-zA-Z0-9,-\.\s]+$'
+def has_non_tokenizable_characters(file_path):
+    non_tokenizable_characters = ["\u2018", "\u00A0", "\u201C", "\u201C"]  # List of non-tokenizable characters
 
-# open the input file and read each line
-with open(sys.argv[1], 'r') as f:
-    for line in f:
-        # check if the line contains any non-matching characters
-        if not re.match(pattern, line):
-            print(line)
+    with open(file_path, "r", encoding="utf-8") as file:
+        content = file.read()
+        for character in non_tokenizable_characters:
+            if character in content:
+                return True
+
+    return False
+
+
+# Get a list of file paths matching the pattern "clean_*.txt"
+file_paths = glob.glob("clean_*.txt")
+
+# Iterate over the file paths and check for non-tokenizable characters
+for file_path in file_paths:
+    if has_non_tokenizable_characters(file_path):
+        print(f"File '{file_path}' contains non-tokenizable characters.")
+    else:
+        print(f"File '{file_path}' does not contain non-tokenizable characters.")
