5e6d3cfb63384715c97e98137cdfaa3413425e04
Yerong <yerong.li@outlook.com>
2023-07-03 23:22:47 -0500

    Update

--- tmp/Harry_Potter_Movies/generate.py
+++ tmp/Harry_Potter_Movies/generate.py
@@ -3,7 +3,6 @@ import pandas as pd
 # Define the file paths
 dialogue_file = 'Dialogue.csv'
 character_file = 'Characters.csv'
-output_file = 'clean.txt'
 
 # Load character map
 character_map = {}
@@ -14,14 +13,35 @@ for index, row in character_df.iterrows():
     character_name = row['Character Name']
     character_map[character_id] = character_name
 
-# Read dialogue file in order
+# Read dialogue file
 dialogue_df = pd.read_csv(dialogue_file, encoding='unicode_escape')
 
-# Write dialogue to the output file
-with open(output_file, 'w') as file:
-    for index, row in dialogue_df.iterrows():
-        character_id = row['Character ID']
-        dialogue = row['Dialogue']
-        character_name = character_map.get(character_id)
+# Sort dialogue dataframe by Chapter ID and Place ID
+dialogue_df = dialogue_df.sort_values(by=['Chapter ID', 'Place ID'])
+
+# Counter for file names
+file_counter = 1
+
+# Variables to track the previous Chapter ID and Place ID
+previous_chapter_id = None
+previous_place_id = None
+
+# Iterate over the dialogue rows
+for index, row in dialogue_df.iterrows():
+    chapter_id = row['Chapter ID']
+    place_id = row['Place ID']
+    dialogue = row['Dialogue']
+    character_id = row['Character ID']
+    character_name = character_map.get(character_id)
+
+    # Check if Chapter ID or Place ID changed
+    if chapter_id != previous_chapter_id or place_id != previous_place_id:
+        output_file = f'clean_{file_counter:02d}.txt'
+        file_counter += 1
+        previous_chapter_id = chapter_id
+        previous_place_id = place_id
+
+    # Write dialogue to the current output file
+    with open(output_file, 'a') as file:
         if character_name:
             file.write(f'[{character_name}] {dialogue}\n')
