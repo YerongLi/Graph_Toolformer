19e818987c9c0b5ddf2eb92c734194ad40e5827a
Yerong <yerong.li@outlook.com>
2023-07-03 20:10:08 -0500

    tmp

--- tmp/tensor_p.py
+++ tmp/tensor_p.py
@@ -1,7 +1,10 @@
 import transformers
 import tensor_parallel as tp
-tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
-model = transformers.AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-2.7B")  # use opt-125m for testing
+model_name_or_path = '/scratch/yerong/.cache/pyllama/hf/7B'
+# tokenizer = transformers.AutoTokenizer.from_pretrained("EleutherAI/gpt-neo-2.7B")
+tokenizer = transformers.AutoTokenizer.from_pretrained(model_name_or_path)
+# model = transformers.AutoModelForCausalLM.from_pretrained?("EleutherAI/gpt-neo-2.7B")  # use opt-125m for testing
+model = transformers.AutoModelForCausalLM.from_pretrained?(model_name_or_path)  # use opt-125m for testing
 
 model = tp.tensor_parallel(model, ["cuda:0", "cuda:1","cuda:2", "cuda:3"])  # <- each GPU has half the weights
 
