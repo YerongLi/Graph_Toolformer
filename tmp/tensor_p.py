87be6693256d05f9846171a6ed51f1df5d934c84
Yerong <yerong.li@outlook.com>
2023-07-03 19:38:01 -0500

    4 GPUs

--- tmp/tensor_p.py
+++ tmp/tensor_p.py
@@ -3,7 +3,7 @@ import tensor_parallel as tp
 tokenizer = transformers.AutoTokenizer.from_pretrained("facebook/opt-13b")
 model = transformers.AutoModelForCausalLM.from_pretrained("facebook/opt-13b")  # use opt-125m for testing
 
-model = tp.tensor_parallel(model, ["cuda:0", "cuda:1"])  # <- each GPU has half the weights
+model = tp.tensor_parallel(model, ["cuda:0", "cuda:1","cuda:2", "cuda:3"])  # <- each GPU has half the weights
 
 inputs = tokenizer("A cat sat", return_tensors="pt")["input_ids"].to("cuda:0")
 outputs = model.generate(inputs, num_beams=5)
