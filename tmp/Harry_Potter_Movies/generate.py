7c1e5353c75b49318abd97afff7a9c98a07ade9c
Yerong <yerong.li@outlook.com>
2023-07-03 23:10:58 -0500

    in the dialogue.csv order

--- tmp/Harry_Potter_Movies/generate.py
+++ tmp/Harry_Potter_Movies/generate.py
@@ -3,31 +3,28 @@ import pandas as pd
 # Define the file paths
 dialogue_file = 'Dialogue.csv'
 character_file = 'Characters.csv'
-output_file = 'dialogue.txt'
+output_file = 'Harry.txt'
 
 # Load character map
 character_map = {}
-character_df = pd.read_csv(character_file, encoding = 'unicode_escape')
+character_df = pd.read_csv(character_file, encoding='unicode_escape')
 
 for index, row in character_df.iterrows():
     character_id = row['Character ID']
     character_name = row['Character Name']
     character_map[character_id] = character_name
 
-# Extract dialogue for all characters
-character_dialogue = {}
-dialogue_df = pd.read_csv(dialogue_file, encoding = 'unicode_escape')
-for index, row in dialogue_df.iterrows():
-    character_id = row['Character ID']
-    dialogue = row['Dialogue']
-    if character_id not in character_dialogue:
-        character_dialogue[character_id] = []
-    character_dialogue[character_id].append(dialogue)
+# Read dialogue file in order
+dialogue_df = pd.read_csv(dialogue_file, encoding='unicode_escape')
+
+# Filter dialogue for Harry Potter (Character ID: 1)
+harry_dialogue_df = dialogue_df[dialogue_df['Character ID'] == 1]
 
 # Write dialogue to the output file
 with open(output_file, 'w') as file:
-    for character_id, dialogues in character_dialogue.items():
+    for index, row in harry_dialogue_df.iterrows():
+        character_id = row['Character ID']
+        dialogue = row['Dialogue']
         character_name = character_map.get(character_id)
         if character_name:
-            for dialogue in dialogues:
-                file.write(f'[{character_name}] {dialogue}\n')
+            file.write(f'[{character_name}] {dialogue}\n')
