d15c21f8cee0fe13ca9b4dbcbe588a562a438302
Yerong <yerong.li@outlook.com>
2023-07-12 21:59:42 -0500

    push

--- tmp/api.py
+++ tmp/api.py
@@ -3,7 +3,7 @@ from gradio_client import Client
 client = Client("https://umiuni-harry-potter.hf.space/")
 result = client.predict(
 				"Hi, what's your name!",	# str  in 'parameter_6' Textbox component
-				'null',
+				'data.json',
 				2000,	# int | float (numeric value between 0 and 4096) in 'Maximum length' Slider component
 				0.8,	# int | float (numeric value between 0 and 1) in 'Top P' Slider component
 				0.1,	# int | float (numeric value between 0 and 1) in 'Temperature' Slider component
