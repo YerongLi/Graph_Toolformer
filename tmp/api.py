8fe2e400152defdf625b119963d2da45c2b59e4c
Yerong <yerong.li@outlook.com>
2023-07-16 18:20:43 -0500

    api last response

--- tmp/api.py
+++ tmp/api.py
@@ -4,13 +4,13 @@ client = Client("https://umiuni-harry-potter.hf.space/")
 result = client.predict(
 				"Hi, what's your name!",	# str  in 'parameter_6' Textbox component
 				2000,	# int | float (numeric value between 0 and 4096) in 'Maximum length' Slider component
-				0.8,	# int | float (numeric value between 0 and 1) in 'Top P' Slider component
-				0.1,	# int | float (numeric value between 0 and 1) in 'Temperature' Slider component
+				0.25,	# int | float (numeric value between 0 and 1) in 'Top P' Slider component
+				0.95,	# int | float (numeric value between 0 and 1) in 'Temperature' Slider component
 			    api_name="/predict"
 
 )
-print(result)
-# print(type(result))
+print(result[-1])
+print(type(result))
 
 
 # from gradio_client import Client
