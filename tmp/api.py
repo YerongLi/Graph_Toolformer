f93f64efe8d2c4b9b4011ae1d73c92392ade9de9
Yerong <yerong.li@outlook.com>
2023-07-16 16:37:58 -0500

    Upload

--- tmp/api.py
+++ tmp/api.py
@@ -15,7 +15,7 @@
 
 from gradio_client import Client
 
-client = Client("https://6027c2047270a705aa.gradio.live/")
+client = Client("https://70f64738fbdcce365d.gradio.live/")
 result = client.predict(
 				"Howdy!",	# str  in 'input' Textbox component
 				0,	# int | float (numeric value between 0 and 4096) in 'Maximum length' Slider component
@@ -24,3 +24,4 @@ result = client.predict(
 				api_name="/predict"
 )
 print(result)
+print(type(result))
