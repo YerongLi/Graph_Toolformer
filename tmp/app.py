1dd7617a7df2d1e043d2f50785750c0c1f85bcbd
Yerong <yerong.li@outlook.com>
2023-07-17 04:10:11 -0500

    json as input

--- tmp/app.py
+++ tmp/app.py
@@ -239,19 +239,19 @@ with gr.Blocks() as demo:
 
     chatbot = gr.Chatbot()
 
-    with gr.row():
-        with gr.column(scale=4):
-            with gr.column(scale=12):
-                user_input = gr.textbox(show_label=False, placeholder="Input...", lines=10).style(container=False)
-            with gr.column(min_width=32, scale=1):
+    with gr.Row():
+        with gr.Column(scale=4):
+            with gr.Column(scale=12):
+                user_input = gr.Textbox(show_label=False, placeholder="Input...", lines=10).style(container=False)
+            with gr.Column(min_width=32, scale=1):
                 submitBtn = gr.button("Submit", variant="primary")
         with gr.column(scale=1):
-            emptyBtn = gr.button("Clear History")
-            max_length = gr.slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
-            top_p = gr.slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
-            temperature = gr.slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
+            emptyBtn = gr.Button("Clear History")
+            max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
+            top_p = gr.Slider(0, 1, value=0.7, step=0.01, label="Top P", interactive=True)
+            temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
 
-    history = gr.state([])
+    history = gr.State([])
 
     submitBtn.click(predict, [user_input, chatbot, max_length, top_p, temperature, history], [chatbot, history], show_progress=True)
     submitBtn.click(reset_user_input)
