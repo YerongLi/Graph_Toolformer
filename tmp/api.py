8b2048f6f57a7e95ade451f8d0e7560c3b696c21
Yerong <yerong.li@outlook.com>
2023-07-17 04:20:41 -0500

    json as input

--- tmp/api.py
+++ tmp/api.py
@@ -1,8 +1,9 @@
 import requests
 
-requests.post(
+data = requests.post(
     url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 0}
 ).json()
+print(data)
 requests.post(
     url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 1}
 ).json()
