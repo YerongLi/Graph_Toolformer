5cece6ea0c406383d35857c2b63c1d94b2cc8bf5
Yerong <yerong.li@outlook.com>
2023-07-17 02:55:30 -0500

    Change url

--- tmp/api.py
+++ tmp/api.py
@@ -1,8 +1,8 @@
 from gradio_client import Client
 
-client = Client("https://d71322c8759610f657.gradio.live/")
+client = Client("https://0c53c70ee482bf1dc0.gradio.live")
 result = client.predict(
-				"Howdy!",	# str  in 'parameter_3' Textbox component
+				"Yerong",	# str  in 'parameter_3' Textbox component
 				fn_index=0
 )
 print(result)
