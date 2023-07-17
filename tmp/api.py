234a3f1f199c0aad29d48d340e3ed58d80fb71f8
Yerong <yerong.li@outlook.com>
2023-07-17 03:03:47 -0500

    Update url

--- tmp/api.py
+++ tmp/api.py
@@ -1,9 +1,16 @@
-from gradio_client import Client
+# from gradio_client import Client
 
-client = Client("https://368f09ca0c818b44d0.gradio.live")
-result = client.predict(
-				"Yerong",	# str  in 'parameter_3' Textbox component
-				fn_index=0
-)
-print(result)
-print(type(result))
+# client = Client("https://368f09ca0c818b44d0.gradio.live")
+# result = client.predict(
+# 				"Yerong",	# str  in 'parameter_3' Textbox component
+# 				fn_index=0
+# )
+# print(result)
+# print(type(result))
+
+response = requests.post("https://tomsoderlund-rest-api-with-gradio.hf.space/run/predict", json={
+  "data": [
+    "hello world",
+]}).json()
+
+data = response["data"]
