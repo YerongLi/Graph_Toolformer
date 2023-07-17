fc5463d4a2878cbb899f46f6080c4f2d7eaca852
Yerong <yerong.li@outlook.com>
2023-07-17 02:53:17 -0500

    Test api.py

--- tmp/api.py
+++ tmp/api.py
@@ -1,8 +1,8 @@
 from gradio_client import Client
 
-client = Client("https://gradio-blocks-hello.hf.space/")
+client = Client("https://d71322c8759610f657.gradio.live/")
 result = client.predict(
-				"Howdy!",	# str  in 'parameter_2' Textbox component
+				"Howdy!",	# str  in 'parameter_3' Textbox component
 				fn_index=0
 )
 print(result)
