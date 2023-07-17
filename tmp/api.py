5e4aa4eb1a61e1f81f31c9036daeb44755836188
Yerong <yerong.li@outlook.com>
2023-07-17 04:20:07 -0500

    json as input

--- tmp/api.py
+++ tmp/api.py
@@ -1,33 +1,8 @@
-# # from gradio_client import Client
-# Make a request to the Gradio API endpoint
 import requests
-# Make a request to the Gradio API endpoint
-response = requests.post("http://127.0.0.1:7860/api/predict", json={
-    "data": [{"query" : "hello world", "history": ['tes']}],
-}).json()
 
-print(response)
-data = response["data"]
-print(data)
-print(type(data))
-
-# # response = requests.post("https://tomsoderlund-rest-api-with-gradio.hf.space/run/predict", json={
-# #   "data": [
-# #     "hello world",
-# # ]}).json()
-
-# # data = response["data"]
-# # print(data)
-# # print(type(data))
-
-# import requests
-
-# data = requests.post(
-#     url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 0}
-# ).json()
-# print(data)
-
-# data = requests.post(
-#     url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 1}
-# ).json()
-# print(data)
+requests.post(
+    url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 0}
+).json()
+requests.post(
+    url="https://hf.space/embed/versae/gradio-blocks-rest-api/+/api/predict/", json={"data": ["Jessie"], "fn_index": 1}
+).json()
