8ba035f2507090cc3f2e9d1c595938b8b6d9509d
Yerong Li <yerong.li@outlook.com>
2023-08-18 23:12:38 -0500

    fetch-lfs.py

---
+++ tmp/fetch_lfs.py
@@ -0,0 +1,19 @@
+from huggingface_hub import snapshot_download
+
+# Set your Hugging Face API token
+hf_token = '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'
+
+# Set repository information
+repo_id = "meta-llama/Llama-2-70b"
+local_folder = "/scratch/yerong/.cache/pyllama/Llama-2-70b"
+
+# Download the snapshot
+snapshot_download(
+    repo_id=repo_id,
+    local_dir=local_folder,
+    use_auth_token=hf_token,
+    local_dir_use_symlinks=False
+)
+
+print("Download complete.")
+
